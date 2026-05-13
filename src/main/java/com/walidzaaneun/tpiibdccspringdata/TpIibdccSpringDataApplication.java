f451f1eab8ca7a357f2b61cb6fdb30efa2fa31d7
walidzaaneun <zaanoun3@gmail.com>
2026-05-13 19:28:21 +0100

    add data for test with CommandeLineRunner

--- src/main/java/com/walidzaaneun/tpiibdccspringdata/TpIibdccSpringDataApplication.java
+++ src/main/java/com/walidzaaneun/tpiibdccspringdata/TpIibdccSpringDataApplication.java
@@ -1,7 +1,13 @@
 package com.walidzaaneun.tpiibdccspringdata;
 
+import com.walidzaaneun.tpiibdccspringdata.entities.Product;
+import com.walidzaaneun.tpiibdccspringdata.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class TpIibdccSpringDataApplication {
@@ -10,4 +16,33 @@ public class TpIibdccSpringDataApplication {
         SpringApplication.run(TpIibdccSpringDataApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository) {
+        return args -> {
+            Product p1 = Product.builder()
+                    .name("Smartphone")
+                    .price(1000)
+                    .quantity(20)
+                    .build();
+            Product p2 = Product.builder()
+                    .name("Speaker")
+                    .price(300)
+                    .quantity(17)
+                    .build();
+            Product p3 = Product.builder()
+                    .name("TV")
+                    .price(2000)
+                    .quantity(35)
+                    .build();
+
+            productRepository.save(p1);
+            productRepository.save(p2);
+            productRepository.save(p3);
+
+            List<Product> products = productRepository.findAll();
+            products.forEach(
+                    System.out::println
+            );
+        };
+    }
 }
