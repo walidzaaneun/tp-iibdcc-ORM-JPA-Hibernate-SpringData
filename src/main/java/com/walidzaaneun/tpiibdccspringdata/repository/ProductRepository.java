862f6c489a515702c345f4e617d4ad326a8f6292
walidzaaneun <zaanoun3@gmail.com>
2026-05-13 19:53:16 +0100

    add findByName keyword

--- src/main/java/com/walidzaaneun/tpiibdccspringdata/repository/ProductRepository.java
+++ src/main/java/com/walidzaaneun/tpiibdccspringdata/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.walidzaaneun.tpiibdccspringdata.repository;
 import com.walidzaaneun.tpiibdccspringdata.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findByNameContains(String keyword);
 }
