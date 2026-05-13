f29a8423a959df09868698d8e929ac611ff914a9
walidzaaneun <zaanoun3@gmail.com>
2026-05-13 18:57:36 +0100

    project init

---
+++ src/main/java/com/walidzaaneun/tpiibdccspringdata/TpIibdccSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.walidzaaneun.tpiibdccspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIibdccSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpIibdccSpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/walidzaaneun/tpiibdccspringdata/TpIibdccSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walidzaaneun.tpiibdccspringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIibdccSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
